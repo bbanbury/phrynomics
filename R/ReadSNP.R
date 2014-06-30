a1192274932e38a31f5b202f1a505eece16108ab
Barb Banbury <darwinthesun@gmail.com>
2014-06-30 11:18:39 -0700

    reworking R code to do roxygen

--- R/ReadSNP.R
+++ R/ReadSNP.R
@@ -1,3 +1,26 @@
+#' Read SNP
+#' 
+#' Read a SNP File
+#'
+#' This function reads in SNP datasets into R from nexus or phylip formatted files. 
+#'
+#' @alias snp
+#' @param file A file name specified by either a variable or a double-quoted string (ie, file="")
+#' @param row.names A numeric indicating which column of data the row names occur (default is 1)
+#' @export
+#' @return Returns an object in the class "snp" with the following components:
+#' @item data The SNP data matrix
+#' @item ntax Number of total taxa present in data matrix
+#' @item nloci Number of loci present in the data matrix
+#' @item nsnps A vector of the number of SNPs present in each locus
+#' @seealso \link{WriteSNP} 
+#' @author B.Banbury
+#' @examples
+#' 
+#' a <- matrix(data=c("AANNTGG", "AATTTGC", "TAAATGC"), dimnames=list(c("A", "B", "C"), "locus"))
+#' write.table(a, file="ex.data", col.names=FALSE)
+#' ReadSNP("ex.data")
+
 ReadSNP <- function(file, row.names=1){
   inputFileType <- FileFormat(file)
   if(inputFileType == "phy")
--- R/print.snp.R
+++ R/print.snp.R
@@ -1,5 +1,5 @@
 print.snp <- function (x, ...){
   cat(paste("\nSNP dataset contains",  x$ntax, "taxa,", x$nloci, "loci, and", sum(x$nsnps), "SNPs\n\n"))
   cat("Taxa include:\n")
-  cat(paste("\t", paste(rownames(x$data)[1:min(6, ntax)], collapse = ", "), ", ...\n", sep = ""))
+  cat(paste("\t", paste(rownames(x$data)[1:min(6, x$ntax)], collapse = ", "), ", ...\n", sep = ""))
 }
