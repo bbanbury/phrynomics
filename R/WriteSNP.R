3b66c6d2e9b10b406c4d7b2d08b5c796e4e7cecc
Barb Banbury <darwinthesun@gmail.com>
2014-07-16 11:29:15 -0700

    a few more rd blocks added

--- R/WriteSNP.R
+++ R/WriteSNP.R
@@ -4,7 +4,7 @@
 #'
 #' This function writes SNP datasets into nexus or phylip formatted files. 
 #'
-#' @param data A data matrix of the class "matrix", "data.frame", or "snp"
+#' @param SNPdataset A data matrix of the class "matrix", "data.frame", or "snp"
 #' @param file A file name specified by either a variable or a double-quoted string. If not specified (ie, file=""), then snp data is printed to the console. 
 #' @param format Format for file, either "nexus" or "phylip"
 #' @param missing A character denoting missing data, usually "N", "?", or "-"
@@ -19,25 +19,25 @@
 #' data(fakeData)
 #' WriteSNP(fakeData, file="refakeData.nex", format="nexus")
 
-WriteSNP <- function(data, file="", format="phylip", missing="N"){
-  if(class(data) == "snp")
-    data <- data$data
+WriteSNP <- function(SNPdataset, file="", format="phylip", missing="N"){
+  if(class(SNPdataset) == "snp")
+    SNPdataset <- SNPdataset$data
   if(format == "phylip"){
-    write(c(dim(data)[1], sum(nchar(data[1,]))), file=file)
-    write.table(data, file=file, quote=FALSE, append=TRUE, col.names=FALSE)
+    write(c(dim(SNPdataset)[1], sum(nchar(SNPdataset[1,]))), file=file)
+    write.table(SNPdataset, file=file, quote=FALSE, append=TRUE, col.names=FALSE)
   }
   if(format == "nexus"){
-    if(class(data) == "data.frame" || class(data) == "matrix")
-      if(dim(data)[2] > 1)
-        data <- data.frame(apply(data, 1, paste, collapse=""))
-    nchars <- min(apply(data, 1, nchar))
+    if(class(SNPdataset) == "data.frame" || class(SNPdataset) == "matrix")
+      if(dim(SNPdataset)[2] > 1)
+        SNPdataset <- data.frame(apply(SNPdataset, 1, paste, collapse=""))
+    nchars <- min(apply(SNPdataset, 1, nchar))
     write(paste("#NEXUS"), file)
     write(paste("[Written ", Sys.Date(), " via phrynomics]", sep=""), file, append=TRUE)
     write(paste("BEGIN Data;"), file, append=TRUE)
-    write(paste("   DIMENSIONS NTAX=", length(data), " NCHAR=", nchars, ";", sep=""), file, append=TRUE)
+    write(paste("   DIMENSIONS NTAX=", length(SNPdataset), " NCHAR=", nchars, ";", sep=""), file, append=TRUE)
     write(paste("   FORMAT DATATYPE=Standard INTERLEAVE=no missing=", missing, ";", sep=""), file, append=TRUE)
     write(paste("Matrix"), file, append=TRUE)
-    write.table(data, file, append=TRUE, quote=FALSE, col.names=FALSE)  
+    write.table(SNPdataset, file, append=TRUE, quote=FALSE, col.names=FALSE)  
     write(paste(""), file, append=TRUE)
     write(paste(";"), file, append=TRUE)
     write(paste("END;"), file, append=TRUE)
--- R/removeOutgroups.R
+++ R/removeOutgroups.R
@@ -1,4 +1,17 @@
+#' Remove Outgroups
+#' 
+#' This function will remove taxon names that do not match with an ingroup ID.  FOr example, if all the ingroup taxa share a taxon flag identifier "PH", then this function will remove any that do not have this flag. Useful for comparing ingroup to whole tree data or for analyses like SNAPP that do not require ingroups.
+#' @param SNPdataset SNP data in the class "matrix", "data.frame", or "snp"
+#' @param taxon Unique flag for ingroup taxa
+#' @export
+#' @return Returns a subset dataset with only ingroup taxa
+#' @seealso \link{ReadSNP} \link{WriteSNP}
+#' @examples
+#' data(fakeData)
+#' removeOutgroups(fakeData, taxon="in")
+
 removeOutgroups <- function(SNPdataset, taxon="PH"){
-#probably only relevant to our study
+  if(class(SNPdataset) == "snp")
+    SNPdataset <- SNPdataset$data
   return(SNPdataset[grep(taxon, rownames(SNPdataset)),])
 }
