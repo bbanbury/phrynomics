49e81251efb1e11cbbfde2c62b399090a7ad03fe
Barb Banbury <darwinthesun@gmail.com>
2014-07-21 12:16:35 -0700

    a few changes to rd formatting

--- R/FileFormat.R
+++ R/FileFormat.R
@@ -15,7 +15,6 @@
 #' FileFormat("example.nex")
 
 FileFormat <- function(file){
-  # returns "phy" or "nex" depending on the file type
   format <- NA
   if(any(grep("Error", try(read.table(file, skip=GetLinesToSkip(file)), silent=TRUE), ignore.case=TRUE))) {
     if(!any(grep("Error", try(read.nexus.data(file)), ignore.case=TRUE)))
