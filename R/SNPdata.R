036cca9155a0d22ce3c456065dbb684da5f41263
Barb Banbury <darwinthesun@gmail.com>
2014-07-24 12:10:34 -0700

    added new NAMESPACE

---
+++ R/SNPdata.R
@@ -0,0 +1,7 @@
+#' SNP data
+#' 
+#' A subset of real phrynosoma SNP data. The full dataset contains 73 individuals, but this subset is only the first 10.  This dataset was originally created using pyrad, and was restricted to loci that had data for at least 60 of the 73 individuals (fairly low amounts of missing data).  
+#' docType data
+#' @keywords datasets
+#' @name SNPdata
+NULL
