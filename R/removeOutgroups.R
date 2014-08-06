c9dd2d66a502adcb21835ea34d6a8e248f5b98d9
Barb Banbury <darwinthesun@gmail.com>
2014-08-05 22:07:06 -0700

    changed name of removeOutgroups to removeGroups

--- R/removeOutgroups.R
+++ R/removeOutgroups.R
@@ -2,7 +2,7 @@
 #' 
 #' This function will remove taxon names that do not match with an ingroup ID.  FOr example, if all the ingroup taxa share a taxon flag identifier "PH", then this function will remove any that do not have this flag. Useful for comparing ingroup to whole tree data or for analyses like SNAPP that do not require ingroups.
 #' @param SNPdataset SNP data in the class "matrix", "data.frame", or "snp"
-#' @param taxon Unique flag for ingroup taxa
+#' @param groupFlag Unique flag for ingroup taxa
 #' @export
 #' @return Returns a subset dataset with only ingroup taxa
 #' @seealso \link{ReadSNP} \link{WriteSNP}
@@ -10,12 +10,12 @@
 #' data(fakeData)
 #' removeOutgroups(fakeData, taxon="in")
 
-removeOutgroups <- function(SNPdataset, taxon="PH"){
+removeGroup <- function(SNPdataset, groupFlag=""){
   if(class(SNPdataset) == "snp"){
-    newData <- SNPdataset$data[grep(taxon, rownames(SNPdataset$data)),]
+    newData <- SNPdataset$data[grep(groupFlag, rownames(SNPdataset$data)),]
     newData <- ReadSNP(newData)
     return(newData)
   }
   else
-    return(SNPdataset[grep(taxon, rownames(SNPdataset)),])
+    return(SNPdataset[grep(groupFlag, rownames(SNPdataset)),])
 }
