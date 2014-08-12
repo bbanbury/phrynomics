b322ab8915cb29e5bb1823ccd733fb976b021d1b
Barb Banbury <darwinthesun@gmail.com>
2014-08-12 12:08:37 -0700

    removed full pop from ExportPops

--- R/ExportPops.R
+++ R/ExportPops.R
@@ -4,6 +4,7 @@
 #' @param SNPdataset SNP data in the class "matrix", "data.frame", or "snp"
 #' @param file A file name specified by either a variable or a double-quoted string. If not specified (ie, file=""), then  data is printed to the console. 
 #' @param subsets A vector of flags which will subset the populations. 
+#' @param includeFull logical, to include the full population data in addition to the subsets.  
 #' @param ... additional arguments passed to \code{write.table}
 #' @export
 #' @return Returns a table with allele counts for a whole population and any subset populations. 
@@ -14,12 +15,12 @@
 #' #fakeData <- AddAFlag(fakeData, flagToAdd="WEST", taxa=c("in1", "in2", "in3"))
 #' #ExportPops(fakeData, subsets=c("WEST"))
 
-ExportPops <- function(SNPdataset, file="", subsets=c(), ...){
+ExportPops <- function(SNPdataset, file="", subsets=c(), includeFull=FALSE, ...){
 #this will print to screen and write to file a treemix file.
 #subsets should be the flags ex: c("north", "west").  Will write full pop then n subsets
   if(class(SNPdataset) == "snp")
     SNPdataset <- SNPdataset$data
-  alleleCounts <- lapply(SplitSNP(SNPdataset), ReturnAlleleCounts)
+  alleleCounts <- lapply(SplitSNP(cSNP(SNPdataset)), ReturnAlleleCounts)
   pops <- matrix(ncol=1+length(subsets), nrow=length(alleleCounts))
   newRownames <- NULL
   for(i in sequence(dim(pops)[1])){
@@ -30,13 +31,15 @@ ExportPops <- function(SNPdataset, file="", subsets=c(), ...){
   colnames(pops) <- c("full", subsets)
     for(j in sequence(length(subsets))){
       subsetPop <- RemoveGroups(SNPdataset, groupFlag=subsets[j])
-      alleleCounts <- lapply(SplitSNP(subsetPop), ReturnAlleleCounts)
+      alleleCounts <- lapply(SplitSNP(cSNP(subsetPop)), ReturnAlleleCounts)
       alleleCounts <- addazero(alleleCounts, rownames(pops))
       for(row in sequence(dim(pops)[1])){
         pops[row,j+1] <- paste(alleleCounts[[row]], collapse=",")
       }
     }
+  if(!includeFull)
+    pops <- pops[,-1]  
   if(!is.null(file))
     write.table(pops, file=file, row.names=FALSE, quote=FALSE, ...)
-  return(pops)  
+  return(pops)
 }
