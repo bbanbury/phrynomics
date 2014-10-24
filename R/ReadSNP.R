da81e5dfb9be284456397c31aecd638a20b2e4ba
Barb Banbury <darwinthesun@gmail.com>
2014-10-24 11:13:46 -0700

    removed GetLinesToSkip plus 1

--- R/ReadSNP.R
+++ R/ReadSNP.R
@@ -32,7 +32,7 @@ ReadSNP <- function(file, row.names=1, preprocess=TRUE){
     inputFileType <- FileFormat(file)
     if(inputFileType == "phy"){
       #add a system grep to file here to check for interleaving
-      initializeTable <- read.table(file, row.names=row.names, skip=GetLinesToSkip(file)+1, stringsAsFactors=FALSE, colClasses=c("character"))
+      initializeTable <- read.table(file, row.names=row.names, skip=GetLinesToSkip(file), stringsAsFactors=FALSE, colClasses=c("character"))
     }
     if(inputFileType == "nex") 
       initializeTable <- ConvertNexDataToPhyData(read.nexus.data(file))
